ada02e431c840b0f91691423c210c411e5088e69
Python Coder <151621155+MueedPythonCodes@users.noreply.github.com>
2025-04-17 05:24:45 +0500

    Cofee_Machine.py

--- day15_cofee_machine_project.py
+++ day15_cofee_machine_project.py
@@ -1,64 +1,94 @@
-from day15_cofee_machine_data import MENU,resources
 
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
 
-def check_coins():
-    """Processing coins"""
-    print("Please insert the Coins")
-    dollars=int(input("How many quarters?: ")) * 0.25
-    dollars+=int(input("How many dimes?: ")) * 0.1
-    dollars+=int(input("How many nickles?: ")) * 0.05
-    dollars=int(input("How many pennies?: ")) * 0.01
-    # do/llars=round(dollars,2)
-    return dollars
-
-
-def quantity(ingredients):
-    """Checking whether resources are enough or not"""
-    sufficient=True
-    for key in ingredients:
-        if ingredients[key]>=resources[key]:
-            print(f"Coffee Machine does not have enough {key}")
-            return False
-    
-    return sufficient
-
-
-
-def successful_purchase(amount,menu):
-    if amount>=menu['cost']:
-        remaining=round(amount-menu["cost"],2)
-        print(f"Here is ${remaining} in change.")
-        global money
-        money += MENU[user_input]['cost']
-        return True
-    else:
-        print("You do not have enough money.Money refunded")
-        return False
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
 
 
 
-def start_making_cofee(user_input,ingreients):
-    """This will cut off the required ingredients from  resources"""
-    for key in ingredients:
-        resources[key] -= ingredients[key]
-    print(f"Here is your {user_input}. Enjoy!")
+flag = True
+money = 0
+
+
+def make_coffee(user_desire,money,resources):
+    sed = MENU[user_desire]["ingredients"]
+    for key,value in sed.items():
+        if key in resources:
+            if resources[key] >= value:
+                resources[key] -= value
+            else:
+                return -1
+                
+    money += MENU[user_desire]["cost"]   
+    # print(resources)
+    return money       
+            
 
 
 
-end=True
-money=0
-while end:
-    user_input=input("What would you like? (espresso/latte/cappuccino): ").lower()
-    if user_input=="off":
-        end=False
-    elif user_input=="report":
-        print(f"Water: {resources['water']}ml")
-        print(f"Milk: {resources['milk']}ml")
-        print(f"Cofee: {resources['coffee']}g")
-        print(f"Money: ${money}") 
-    else:
-        ingredients=MENU[user_input]['ingredients']
-        if quantity(ingredients):
-            amount=check_coins()
-            if successful_purchase(amount,MENU[user_input]):
-                start_making_cofee(user_input,ingredients)
+
+while flag:
+    try:
+        user_desire = input("What would you like? (espresso,latte,cappuccino): ").lower()
+        if user_desire == "report":
+            for key,value in resources.items():
+                print(f"{key.capitalize()}: {value}")
+            print(f"Money: ${money}")
+
+        elif user_desire == "latte" or user_desire == "espresso" or user_desire == "cappuccino":
+            print("Please insert coins.")
+            quarters = int(input("How many quarters?")) * 0.25
+            dimes = int(input("How many dimes?")) * 0.10
+            nickels = int(input("How many nickles?")) * 0.05
+            pennies = int(input("How many pennies?")) * 0.01
+            total = quarters+dimes+nickels+pennies
+            # print(f"Total amount user paid is {total}")
+
+            if total > MENU[user_desire]["cost"]:
+                # print(MENU[user_desire]["cost"])
+                coffee_coins = make_coffee(user_desire,money,resources)
+                if coffee_coins == -1:
+                    print(f"Sorry the Coffee Machine is out of resources.")
+                    break
+                else:
+                    money = coffee_coins
+                    total = total - MENU[user_desire]["cost"]
+                    print(f"Here is ${round(total,2)} in change!")
+                    print(f"Here is your {user_desire}.Enjoy!☕🪶")
+        
+        elif user_desire == "off":
+            print("Oops! Sorry.The Coffe Machine is turned off.")
+            break
+
+        else:
+            print("Please enter a valid entry")
+
+    except:
+        print("There might be some error in your input please write Credentials Carefully!")
